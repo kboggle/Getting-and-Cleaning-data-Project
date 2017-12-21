17a398ede654431be764cfca16eceea4186b6913
kboggle <ketki88@gmail.com>
2017-12-21 13:13:12 +0100

    Update Run Analysis.r

--- Run Analysis.r
+++ Run Analysis.r
@@ -1,6 +1,14 @@
 
 library(tidyverse)
+filename <- "getdata_dataset.zip"
 
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
 
 # 1. Merges the training and the test sets to create one data set.
 # test data
